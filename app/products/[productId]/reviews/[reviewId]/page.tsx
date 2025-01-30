a361e7f97990d9e538c49c995dd91ff085d8cba9
Delisa Putri <ddlisap61@gmail.com>
2025-01-31 02:57:22 +0700

    lesson 10

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ reviewId: string; productId: string }>;
 }) {
   const { productId, reviewId } = await params;
+
+  if (+reviewId > 1000) notFound();
+
   return (
     <div>
       Review {reviewId} for product {productId}
